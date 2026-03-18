5c33406b4e73370f2a90141360b6e06c53702e52
JoyD-0303 <148120294+JoyD-0303@users.noreply.github.com>
2026-03-18 07:21:40 -0700

    Revert "Implement database"

--- CS436_RoomMatch/src/views_controllers/LoginPage.java
+++ CS436_RoomMatch/src/views_controllers/LoginPage.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import model.DatabaseManager;
 
 public class LoginPage {
 	private TextField usernameTextField = new TextField();

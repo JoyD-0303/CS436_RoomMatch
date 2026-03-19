d38c822c67ec74b9ea2605c9c647aeb4ea1c8b65
mayaelzein <maya_elzein@icloud.com>
2026-03-19 14:09:53 -0700

    Display saved user preferences on main page

--- CS436_RoomMatch/src/views_controllers/MainPageView.java
+++ CS436_RoomMatch/src/views_controllers/MainPageView.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import model.Observer;
 import model.UserProfile;
 
@@ -12,18 +13,30 @@ public class MainPageView extends BorderPane implements Observer {
 	UserProfile userProfile;
 	
 	public void initializePanel(UserProfile user) {
+		this.setTop(null);
+		this.setCenter(null);
+
 		userProfile = user;
+
 		MenuItem option1 = new MenuItem("option1");
 		MenuItem option2 = new MenuItem("option2");
 		MenuItem option3 = new MenuItem("option3");
 		Menu options = new Menu("Options");
 		options.getItems().addAll(option1, option2, option3);
+
 		MenuBar menuBar = new MenuBar();
 		menuBar.getMenus().addAll(options);
 		this.setTop(menuBar);
 		
-		Label displayInfo = new Label("Welcome " + userProfile.getUser());
-		this.setCenter(displayInfo);
+		Label welcomeLabel = new Label("Welcome " + userProfile.getUser());
+		Label sleepLabel = new Label("Sleep Schedule: " + userProfile.getSleepSchedule());
+		Label cleanlinessLabel = new Label("Cleanliness: " + userProfile.getCleanliness());
+		Label guestsLabel = new Label("Guest Frequency: " + userProfile.getGuests());
+
+		VBox infoBox = new VBox(10);
+		infoBox.getChildren().addAll(welcomeLabel, sleepLabel, cleanlinessLabel, guestsLabel);
+
+		this.setCenter(infoBox);
 	}
 
 	@Override

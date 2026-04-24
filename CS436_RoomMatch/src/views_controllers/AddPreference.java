5c2ed6e5480e9a074978861ae536b1fd0b1595d1
Jordan <italia.france30@gmail.com>
2026-04-23 17:40:19 -0700

    Added code for adding preferences, but it is commented out because code still needs to be written for the main page view and for adding the new preference to the database

--- CS436_RoomMatch/src/views_controllers/AddPreference.java
+++ CS436_RoomMatch/src/views_controllers/AddPreference.java
@@ -2,6 +2,7 @@ package views_controllers;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -31,6 +32,8 @@ public class AddPreference implements Page {
 	private Button savePreferences;
 	@FXML
 	private Button goBack;
+	@FXML
+	private Label information;
 	
 	@Override
 	public void setMainController(RoomMatchGUI source, UserProfile user) {
@@ -55,7 +58,7 @@ public class AddPreference implements Page {
 			line2 = "Options: ";
 			String[] split = file2.nextLine().split(" ");
 			for(int i=0; i<split.length; i++) {
-				line2 += split[i] + (i==split.length-1 ? "" : ", ");
+				line2 += split[i] + (i!=split.length-1 ? ", " : "");
 			}
 			vb.getChildren().add(new Label(line2));
 			vb.getChildren().add(new Label());
@@ -64,6 +67,46 @@ public class AddPreference implements Page {
 		file2.close();
 		
 		scrollPane.setContent(vb);
+	}
+	
+	@FXML
+	private void savePreference(ActionEvent event) throws IOException {
+		System.out.println("Not yet implemented");
+		
+		// Note: Uncomment below to see this in action, just replace the txt files after
+		//       running this. Found in CS436_RoomMatch/txt/...
+		/*
+		String options[] = pOptions.getText().split("\n");
+		String preference = "";
+		
+		information.setText("");
+		
+		if( pDescription.getText().isBlank() || pOptions.getText().isBlank() ) {
+			information.setText("Either the description and/or option text fields are blank.");
+			return;
+		}
+		if( options.length <= 1 ) {
+			information.setText("There should be at least two options provided");
+			return;
+		}
+		
+		writeFile("/txt/descriptions.txt", pDescription.getText());
+		
+		for(int i=0; i<options.length; i++) {
+			preference += options[i].trim().replace(' ', '_') + (i!=options.length-1 ? " " : "");
+		}
+		writeFile("/txt/preferences.txt", preference);
+		
+		pDescription.setText("");
+		pOptions.setText("");
+		
+		controller.setToPage(View.ADDPREF, "Add preferences");
+		*/
+	}
+	
+	@FXML
+	private void returnToMainPage(ActionEvent event) throws IOException {
+		controller.setToPage(View.MAIN, "Welcome");
 	}
 	
 	private Scanner readFile(String path) {
@@ -81,14 +124,18 @@ public class AddPreference implements Page {
 		return file;
 	}
 	
-	@FXML
-	private void savePreference(ActionEvent event) {
-		System.out.println("Not yet implemented");
+	private void writeFile(String path, String save) throws IOException {
+		FileWriter writer;
+		String workingDir = System.getProperty("user.dir");
+		
+		workingDir += path;
+		writer = new FileWriter(workingDir, true);
+		writer.write("\n" + save);
+		writer.close();
 	}
-	
-	@FXML
-	private void returnToMainPage(ActionEvent event) throws IOException {
-		controller.setToPage(View.MAIN, "Welcome");
-	}
-	
 }
+
+
+
+
+
